f15eb30c74e64b59319c38ac3c099a564a7b5470
Mampuru <mampuruntwampe@gmail.com>
2023-12-12 22:58:38 +0200

    Update the function to use from .env file

--- lib/service/aws_cognito_service.dart
+++ lib/service/aws_cognito_service.dart
@@ -1,7 +1,8 @@
 import 'package:amazon_cognito_identity_dart_2/cognito.dart';
+import 'package:flutter_dotenv/flutter_dotenv.dart';
 
 class AwsCognitoService {
-  final userPool = CognitoUserPool('userPoolId', 'clientId');
+  final userPool = CognitoUserPool('${(dotenv.env['POOL_ID'])}', '${(dotenv.env['POOL_ID'])}');
 
   Future<void> login(String email, String password) async {
     final user = CognitoUser(email, userPool);
