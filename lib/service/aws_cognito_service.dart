a412d5f9e5c44601a0a45aa69b05d4ff3edf7224
Mampuru <mampuruntwampe@gmail.com>
2023-12-12 23:23:34 +0200

    Added all the possible cogito exception

--- lib/service/aws_cognito_service.dart
+++ lib/service/aws_cognito_service.dart
@@ -13,6 +13,22 @@ class AwsCognitoService {
     CognitoUserSession? session;
     try{
       session = await user.authenticateUser(authDetails);
+    } on CognitoUserNewPasswordRequiredException catch (e) {
+      // handle New Password challenge
+    } on CognitoUserMfaRequiredException catch (e) {
+      // handle SMS_MFA challenge
+    } on CognitoUserSelectMfaTypeException catch (e) {
+      // handle SELECT_MFA_TYPE challenge
+    } on CognitoUserMfaSetupException catch (e) {
+      // handle MFA_SETUP challenge
+    } on CognitoUserTotpRequiredException catch (e) {
+      // handle SOFTWARE_TOKEN_MFA challenge
+    } on CognitoUserCustomChallengeException catch (e) {
+      // handle CUSTOM_CHALLENGE challenge
+    } on CognitoUserConfirmationNecessaryException catch (e) {
+      // handle User Confirmation Necessary
+    } on CognitoClientException catch (e) {
+      // handle Wrong Username and Password and Cognito Client
     }catch (e){
       if (kDebugMode) {
         print(e);
