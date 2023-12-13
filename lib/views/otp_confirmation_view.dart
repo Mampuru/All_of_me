a40e0011fa0e72afc4a96bc39f7691127481e0bf
Mampuru <mampuruntwampe@gmail.com>
2023-12-13 23:48:49 +0200

    Update with const to the widgets that don't change

--- lib/views/otp_confirmation_view.dart
+++ lib/views/otp_confirmation_view.dart
@@ -1,35 +1,35 @@
 import 'package:flutter/material.dart';
 import 'package:get/get.dart';
 
-import '../controlllers/auth_controller.dart';
-
 class OTPScreen extends StatelessWidget {
   final TextEditingController otpController = TextEditingController();
+
+  OTPScreen({super.key});
 
   @override
   Widget build(BuildContext context) {
     return Scaffold(
       appBar: AppBar(
-        title: Text('OTP Confirmation'),
+        title: const Text('OTP Confirmation'),
       ),
       body: Padding(
-        padding: EdgeInsets.all(16.0),
+        padding: const EdgeInsets.all(16.0),
         child: Column(
           mainAxisAlignment: MainAxisAlignment.center,
           crossAxisAlignment: CrossAxisAlignment.stretch,
           children: [
             TextField(
               controller: otpController,
-              decoration: InputDecoration(labelText: 'Enter OTP'),
+              decoration: const InputDecoration(labelText: 'Enter OTP'),
               keyboardType: TextInputType.number,
             ),
-            SizedBox(height: 20),
+            const SizedBox(height: 20),
             ElevatedButton(
               onPressed: () {
                 // Call the method to verify OTP
                 // Get.find<AuthController>().verifyOTP(otpController.text.trim());
               },
-              child: Text('Verify OTP'),
+              child: const Text('Verify OTP'),
             ),
           ],
         ),
