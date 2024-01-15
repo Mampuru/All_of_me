4277bc865a53e07c94a42f19fce9a2560fd70dda
Mampuru <mampuruntwampe@gmail.com>
2024-01-15 23:58:26 +0200

    Configurattion on the main biew

--- lib/main.dart
+++ lib/main.dart
@@ -1,6 +1,7 @@
 import 'package:all_of_me/constants.dart';
 import 'package:all_of_me/views/sign_in_view.dart';
 import 'package:animated_splash_screen/animated_splash_screen.dart';
+import 'package:auth0_flutter/auth0_flutter.dart';
 import 'package:flutter/material.dart';
 import 'package:flutter_dotenv/flutter_dotenv.dart';
 import 'package:get/get.dart';
@@ -9,15 +10,20 @@ import 'package:get/get.dart';
 import 'controllers/auth_controller.dart';
 
 Future<void> main() async {
+  Auth0 auth0 = Auth0(baseUrl: 'https://<YOUR_AUTH0_DOMAIN>', clientId: '<YOUR_CLIENT_ID>');
+
   await dotenv.load(fileName: ".env");
 
   Get.put(AuthController());
 
-  runApp(const MyApp());
+  runApp(MyApp(auth0: auth0));
 }
 
 class MyApp extends StatelessWidget {
-  const MyApp({super.key});
+
+  final Auth0 auth0;
+
+  const MyApp({required this.auth0});
 
   @override
   Widget build(BuildContext context) {
