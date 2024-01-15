36dbcf4870cafab41d16ef860a68ad3b50cacf1c
Mampuru <mampuruntwampe@gmail.com>
2024-01-15 23:41:59 +0200

    Fixed layout on card view added a scaffold

--- lib/views/card_view.dart
+++ lib/views/card_view.dart
@@ -6,29 +6,31 @@ class CardView extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
-    return const Center(
-      child: Column(
-        mainAxisAlignment: MainAxisAlignment.center,
-        crossAxisAlignment: CrossAxisAlignment.center,
-        children: [
-          InfoCard(
-            title: 'ID Info',
-            info: 'Your ID info here',
-            icon: Icons.credit_card,
-          ),
-          SizedBox(height: 20),
-          InfoCard(
-            title: "Driver's Card",
-            info: 'Driver\'s card info here',
-            icon: Icons.directions_car,
-          ),
-          SizedBox(height: 20),
-          InfoCard(
-            title: 'Passport Info',
-            info: 'Your passport info here',
-            icon: Icons.flight,
-          ),
-        ],
+    return const Scaffold(
+      body: Center(
+        child: Column(
+          mainAxisAlignment: MainAxisAlignment.center,
+          crossAxisAlignment: CrossAxisAlignment.center,
+          children: [
+            InfoCard(
+              title: 'ID Info',
+              info: 'Your ID info here',
+              icon: Icons.credit_card,
+            ),
+            SizedBox(height: 20),
+            InfoCard(
+              title: "Driver's Card",
+              info: 'Driver\'s card info here',
+              icon: Icons.directions_car,
+            ),
+            SizedBox(height: 20),
+            InfoCard(
+              title: 'Passport Info',
+              info: 'Your passport info here',
+              icon: Icons.flight,
+            ),
+          ],
+        ),
       ),
     );
   }
