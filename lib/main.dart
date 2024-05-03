2cac24b5f673951e31c17ea66ef080267b580115
Mampuru <mampuruntwampe@gmail.com>
2024-05-04 00:11:13 +0200

    reomved the 0auth for now

--- lib/main.dart
+++ lib/main.dart
@@ -1,5 +1,5 @@
 import 'package:all_of_me/constants.dart';
-import 'package:all_of_me/views/auth_login_view.dart';
+import 'package:all_of_me/views/sign_in_view.dart';
 import 'package:animated_splash_screen/animated_splash_screen.dart';
 import 'package:auth0_flutter/auth0_flutter.dart';
 import 'package:flutter/material.dart';
@@ -8,14 +8,13 @@ import 'package:flutter_dotenv/flutter_dotenv.dart';
 Future<void> main() async {
   await dotenv.load(fileName: ".env");
 
-  Auth0 auth0 = Auth0(dotenv.env['AUTH0_DOMAIN']!, dotenv.env['AUTH0_CLIENT_ID']!,);
+  // Auth0 auth0 = Auth0(dotenv.env['AUTH0_DOMAIN']!, dotenv.env['AUTH0_CLIENT_ID']!,);
 
-  runApp(MyApp(auth0: auth0));
+  runApp(const MyApp());
 }
 
 class MyApp extends StatelessWidget {
-  const MyApp({super.key, required this.auth0});
-  final Auth0 auth0;
+  const MyApp({super.key, });
 
   @override
   Widget build(BuildContext context) {
@@ -29,7 +28,7 @@ class MyApp extends StatelessWidget {
       home: AnimatedSplashScreen(
           duration: 3000,
           splash: splashLogo,
-          nextScreen: Auth0LoginScreen(auth0: auth0),
+          nextScreen: const LoginScreen(),
           splashTransition: SplashTransition.scaleTransition,
           splashIconSize: 150,
           )
