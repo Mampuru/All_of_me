568afc8937c33b53de0a31075d3a0d77de19fcf6
Mampuru <mampuruntwampe@gmail.com>
2024-05-04 00:21:00 +0200

    changed the layout of sign in view

--- lib/views/sign_in_view.dart
+++ lib/views/sign_in_view.dart
@@ -1,3 +1,4 @@
+import 'package:all_of_me/constants.dart';
 import 'package:flutter/material.dart';
 import 'card_view.dart';
 
@@ -36,45 +37,47 @@ class _LoginScreenState extends State<LoginScreen> with SingleTickerProviderStat
       appBar: AppBar(
         title: const Text('Login'),
       ),
-      body: Padding(
-        padding: const EdgeInsets.all(16.0),
-        child: Column(
-          mainAxisAlignment: MainAxisAlignment.center,
-          crossAxisAlignment: CrossAxisAlignment.stretch,
-          children: [
-            AnimatedOpacity(
-              duration: const Duration(milliseconds: 500),
-              opacity: _fadeAnimation.value,
-              child: Image.asset('assets/logo.png', height: 100, width: 100), // Replace with your logo image
-            ),
-            const SizedBox(height: 20),
-            SlideTransition(
-              position: _slideAnimation,
-              child: TextField(
-                controller: emailController,
-                decoration: const InputDecoration(labelText: 'Email'),
-                keyboardType: TextInputType.emailAddress,
+      body: SingleChildScrollView(
+        child: Padding(
+          padding: const EdgeInsets.fromLTRB(24,24,24,24),
+          child: Column(
+            mainAxisAlignment: MainAxisAlignment.center,
+            crossAxisAlignment: CrossAxisAlignment.stretch,
+            children: [
+              AnimatedOpacity(
+                duration: const Duration(milliseconds: 500),
+                opacity: _fadeAnimation.value,
+                child: Image.asset(splashLogo, height: 100, width: 100), // Replace with your logo image
               ),
-            ),
-            const SizedBox(height: 20),
-            SlideTransition(
-              position: _slideAnimation,
-              child: TextField(
-                controller: passwordController,
-                decoration: const InputDecoration(labelText: 'Password'),
-                obscureText: true,
+              const SizedBox(height: 70),
+              SlideTransition(
+                position: _slideAnimation,
+                child: TextField(
+                  controller: emailController,
+                  decoration: const InputDecoration(labelText: 'Email'),
+                  keyboardType: TextInputType.emailAddress,
+                ),
               ),
-            ),
-            const SizedBox(height: 70),
-            ElevatedButton(
-              onPressed: () {
-                _controller.forward();
-
-                Navigator.of(context).push(MaterialPageRoute(builder: (context) => const CardView()));
-              },
-              child: const Text('Login'),
-            ),
-          ],
+              const SizedBox(height: 20),
+              SlideTransition(
+                position: _slideAnimation,
+                child: TextField(
+                  controller: passwordController,
+                  decoration: const InputDecoration(labelText: 'Password'),
+                  obscureText: true,
+                ),
+              ),
+              const SizedBox(height: 70),
+              ElevatedButton(
+                onPressed: () {
+                  _controller.forward();
+        
+                  Navigator.of(context).push(MaterialPageRoute(builder: (context) => const CardView()));
+                },
+                child: const Text('Login'),
+              ),
+            ],
+          ),
         ),
       ),
     );
