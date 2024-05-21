026935ed2de0450cce71118a47aa192f51346315
Mampuru <mampuruntwampe@gmail.com>
2024-05-21 23:06:03 +0200

    Change the with of the card view

--- lib/views/info_card_view.dart
+++ lib/views/info_card_view.dart
@@ -5,39 +5,48 @@ class InfoCard extends StatelessWidget {
   final String info;
   final IconData icon;
 
-  const InfoCard({super.key,
+  const InfoCard({
+    Key? key,
     required this.title,
     required this.info,
     required this.icon,
-  });
+  }) : super(key: key);
 
   @override
   Widget build(BuildContext context) {
+    // Get the screen width
+    final double screenWidth = MediaQuery.of(context).size.width;
+
     return Card(
       elevation: 4,
       margin: const EdgeInsets.all(20),
-      child: Padding(
+      child: Container(
+        width: screenWidth - 40, // Margin is 20 on each side
         padding: const EdgeInsets.all(20),
-        child: Column(
+        child: Row(
           children: [
             Icon(
               icon,
               size: 50,
               color: Colors.blue,
             ),
-            const SizedBox(height: 10),
-            Text(
-              title,
-              style: const TextStyle(
-                fontSize: 20,
-                fontWeight: FontWeight.bold,
-              ),
-            ),
-            const SizedBox(height: 10),
-            Text(
-              info,
-              textAlign: TextAlign.center,
-              style: const TextStyle(fontSize: 16),
+            const SizedBox(width: 20),
+            Column(
+              crossAxisAlignment: CrossAxisAlignment.start,
+              children: [
+                Text(
+                  title,
+                  style: const TextStyle(
+                    fontSize: 20,
+                    fontWeight: FontWeight.bold,
+                  ),
+                ),
+                const SizedBox(height: 10),
+                Text(
+                  info,
+                  style: const TextStyle(fontSize: 16),
+                ),
+              ],
             ),
           ],
         ),
