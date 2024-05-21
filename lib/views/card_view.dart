4d26503e397677734d400c96173c1b94a64165eb
Mampuru <mampuruntwampe@gmail.com>
2024-05-22 00:53:36 +0200

    Additon of the example data on the card view

--- lib/views/card_view.dart
+++ lib/views/card_view.dart
@@ -1,6 +1,8 @@
 import 'package:all_of_me/views/id_card_view.dart';
 import 'package:flutter/material.dart';
 
+import 'driver_card_view.dart';
+
 class CardView extends StatelessWidget {
   const CardView({super.key});
 
@@ -13,20 +15,26 @@ class CardView extends StatelessWidget {
           crossAxisAlignment: CrossAxisAlignment.center,
           children: [
             IDCard(
-              title: 'I.D No XXXXXX XXXX XXX',
-              surname: 'MAMPURU',
-              forename: "NTWAMPE",
+              title: 'I.D No. XXXXXX XXXX XXX',
+              surname: 'JOHN',
+              forename: "WICK",
               country: "SOUTH AFRICA",
               issueDate: "2012-07-23",
               bob: "2002-06-29",
               icon: Icons.credit_card,
             ),
-            // SizedBox(height: 20),
-            // InfoCard(
-            //   title: "Driver's Card",
-            //   info: 'Driver\'s card info here',
-            //   icon: Icons.directions_car,
-            // ),
+            SizedBox(height: 20),
+            DriverCard(
+              licenceNumber: "No. XXXXXXXXXXXX",
+              country: "ZA",
+              gender: 'M',
+              restriction: "0",
+              valid: "04/05/2023 - 03/05/2028",
+              code: "C1",
+              vehicleRestrictions: "0",
+              issueDate: "22/12/2017",
+              icon: Icons.directions_car,
+            ),
             // SizedBox(height: 20),
             // InfoCard(
             //   title: 'Passport Info',
