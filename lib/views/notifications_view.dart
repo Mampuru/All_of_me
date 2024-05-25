0b21a2759d20deb3537945897a9f7cb13ef98e56
Mampuru <mampuruntwampe@gmail.com>
2024-05-25 04:40:24 +0200

    Updated the notifications layout

--- lib/views/notifications_view.dart
+++ lib/views/notifications_view.dart
@@ -5,6 +5,27 @@ class NotificationView extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
-    return const Placeholder();
+    return Scaffold(
+      appBar: AppBar(
+        title: const Text('Notifications'),
+      ),
+      body: const Column(
+        children: [
+          ListTile(
+            leading: Icon(Icons.message),
+            title: Text("ID Request Action"),
+            trailing: Text("12:00"),
+            subtitle: Text("Application of the Loan at Nedbank"),
+          ),
+          SizedBox(height: 10,),
+          ListTile(
+            leading: Icon(Icons.message),
+            title: Text("ID Request Action"),
+            trailing: Text("10:00"),
+            subtitle: Text("Security check confirmation"),
+          )
+        ],
+      ),
+    );
   }
 }
