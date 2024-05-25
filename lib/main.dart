fcb04e5721a7401dabf396e4873f7658e90f20a7
Mampuru <mampuruntwampe@gmail.com>
2024-05-25 04:41:06 +0200

    Reomved the all the auth0 code

--- lib/main.dart
+++ lib/main.dart
@@ -1,15 +1,10 @@
-import 'package:all_of_me/constants.dart';
+import 'package:all_of_me/views/notifications_view.dart';
 import 'package:all_of_me/views/sign_in_view.dart';
-import 'package:animated_splash_screen/animated_splash_screen.dart';
-import 'package:auth0_flutter/auth0_flutter.dart';
 import 'package:flutter/material.dart';
 import 'package:flutter_dotenv/flutter_dotenv.dart';
 
 Future<void> main() async {
   await dotenv.load(fileName: ".env");
-
-  // Auth0 auth0 = Auth0(dotenv.env['AUTH0_DOMAIN']!, dotenv.env['AUTH0_CLIENT_ID']!,);
-
   runApp(const MyApp());
 }
 
@@ -25,7 +20,7 @@ class MyApp extends StatelessWidget {
         colorScheme: ColorScheme.fromSeed(seedColor: Colors.deepPurple),
         useMaterial3: true,
       ),
-      home: const LoginScreen()
+      home: const NotificationView()
     );
   }
 }
