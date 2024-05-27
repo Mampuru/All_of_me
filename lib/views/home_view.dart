43deebb7fb0939de070f9c4f7c0debd368f20516
Mampuru <mampuruntwampe@gmail.com>
2024-05-27 23:24:14 +0200

    Addition of home view

---
+++ lib/views/home_view.dart
@@ -0,0 +1,57 @@
+import 'package:flutter/material.dart';
+
+class HomeScreen extends StatelessWidget {
+  const HomeScreen({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        title: Text('Home'),
+      ),
+      drawer: Drawer(
+        child: ListView(
+          padding: EdgeInsets.zero,
+          children: <Widget>[
+            DrawerHeader(
+              child: Text(
+                'Menu',
+                style: TextStyle(color: Colors.white, fontSize: 24),
+              ),
+              decoration: BoxDecoration(
+                color: Colors.blue,
+              ),
+            ),
+            ListTile(
+              leading: Icon(Icons.home),
+              title: Text('Home'),
+              onTap: () {
+                Navigator.pop(context); // Close the drawer
+                // Navigate to home screen if necessary
+              },
+            ),
+            ListTile(
+              leading: Icon(Icons.settings),
+              title: Text('Settings'),
+              onTap: () {
+                Navigator.pop(context); // Close the drawer
+                // Navigate to settings screen
+              },
+            ),
+            ListTile(
+              leading: Icon(Icons.contact_page),
+              title: Text('Contact'),
+              onTap: () {
+                Navigator.pop(context); // Close the drawer
+                // Navigate to contact screen
+              },
+            ),
+          ],
+        ),
+      ),
+      body: Center(
+        child: Text('Home Screen'),
+      ),
+    );
+  }
+}
